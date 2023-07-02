faa735ee017753a7541c849ebc7f37a91559bd82
its-me-mano <itsmemano123@gmail.com>
2023-07-02 16:43:19 +0530

    initial commit

---
+++ src/Component/Scoreboard.js
@@ -0,0 +1,9 @@
+import "../index.css";
+const ScoreBoard=({scores})=>{
+    return(
+        <div className="score-board">
+            <h2>{scores}</h2>
+        </div>
+    )
+}
+export default ScoreBoard; 
